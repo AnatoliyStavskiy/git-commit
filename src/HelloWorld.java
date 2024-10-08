7eac7d3bed60b75de7b252bc2755365d5e6f5157
tolbist4@gmail.com <AnatoliyStavskiy>
2024-10-08 21:04:33 +0300

    add new sout

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,5 @@
 class pablic Hello {
     public static void main(String[] args) {
-        System.out.println("Hello worldsdas");
-        System.out.println("Second line");
+        System.out.println("sss");
     }
 }
